09d1aeb91720d048d052eb9dad67cad9683fa1cf
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-20 19:36:04 +0100

    Refactor #hamburger-nav and Nav component:
    - Removed padding from #hamburger-nav for cleaner layout.
    - Adjusted .logo width to 28% for better scaling.
    - Added empty tag element to Nav component function.

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -7,41 +7,43 @@ const Nav = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <nav id="hamburger-nav">
-      <a href="#home">
-        <img className="logo" src={logo} alt="CDR Devs" />
-      </a>
+    <>
+      <nav id="hamburger-nav">
+        <a href="#home">
+          <img className="logo" src={logo} alt="CDR Devs" />
+        </a>
 
-      <div className="hamburger-menu">
-        <Hamburger
-          rounded
-          color="#433D8B"
-          size={35}
-          toggled={isOpen}
-          toggle={setOpen}
-          aria-label="Toggle menu"
-        />
-        <div className={`menu-links ${isOpen ? "open" : ""}`}>
-          <ul>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#our-story">Our Story</a>
-            </li>
-            <li>
-              <a href="#our-team">Our Team</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+        <div className="hamburger-menu">
+          <Hamburger
+            rounded
+            color="#433D8B"
+            size={35}
+            toggled={isOpen}
+            toggle={setOpen}
+            aria-label="Toggle menu"
+          />
+          <div className={`menu-links ${isOpen ? "open" : ""}`}>
+            <ul>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#our-story">Our Story</a>
+              </li>
+              <li>
+                <a href="#our-team">Our Team</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
