7a7641e7cbba297bad86c68601180379605d986a
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-20 16:16:34 +0100

    Add Nav component with hamburger menu and styling

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,34 +1,48 @@
 import "./Nav.css";
+import { useState } from "react";
+import logo from "../assets/images/logo.svg";
+import Hamburger from "hamburger-react";
 
-export default function Nav() {
+const Nav = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <nav>
-      <div>
-        <a href="#home">
-          <img
-            className="logo"
-            src="../public/CDR_Devs_small.png"
-            alt="CDR Devs"
-          />
-        </a>
+    <nav id="hamburger-nav">
+      <a href="#home">
+        <img className="logo" src={logo} alt="CDR Devs" />
+      </a>
+
+      <div className="hamburger-menu">
+        <Hamburger
+          rounded
+          color="#433D8B"
+          size={35}
+          toggled={isOpen}
+          toggle={setOpen}
+          aria-label="Toggle menu"
+        />
+        <div className={`menu-links ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#our-story">Our Story</a>
+            </li>
+            <li>
+              <a href="#our-team">Our Team</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#our-story">Our Story</a>
-        </li>
-        <li>
-          <a href="#our-team">Our Team</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
     </nav>
   );
-}
+};
+
+export default Nav;
