92533b91ddfbe8b9b9111bd9ba5fd2578119368a
csarq <catherinesarquis@gmail.com>
2024-07-21 10:48:55 +0100

    adds about section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,5 +1,26 @@
 import "./About.css";
 
-export default function About() {
-  return <></>;
-}
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Us</h2>
+      <div className="card">
+        <p>
+          Our journey as CDR Devs started with a shared dream: to use our coding
+          skills to have a positive impact on the world 🔥.
+        </p>
+        <p>
+          We're a team of passionate developers who believe that a well-crafted
+          website can be a powerful tool for growth and success.
+        </p>
+        <p>
+          Whether you're a small business or a freelance creative, we're here to
+          help you reach your full online potential.
+        </p>
+        <p>Let's create something extraordinary!</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
