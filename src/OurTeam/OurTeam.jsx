b26d860e02a064c1f5265b51c486ed2734e30a15
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-21 23:40:48 +0100

    Add code block with team members Catherine, David, and Ravi including their images and descriptions in OurTeam component.

--- src/OurTeam/OurTeam.jsx
+++ src/OurTeam/OurTeam.jsx
@@ -1,56 +1,70 @@
-import "./OurTeam.css";
-import catherineImage from "../assets/images/workstation.png";
+import styles from "./OurTeam.module.css";
+import catherineImage from "../assets/images/catherine-small.png";
+import davidImage from "../assets/images/david-small.jpg";
+import raviImage from "../assets/images/ravi-small.jpg";
 
 const OurTeam = () => {
   return (
     <section id="our-team">
       <h2>Meet our team</h2>
-      <div className="our-team-container">
-        <div className="swiper">
-          <div className="swiper-wrapper">
-            <div className="card">
-              <div className="card-image">
-                <img src={catherineImage} alt="catherine's image" />
+      <div className={styles.teamContainer}>
+        <div className={styles.teamSlider}>
+          <div className={styles.teamSliderWrapper}>
+            <div className={styles.teamMemberCard}>
+              <div className={styles.teamMemberImage}>
+                <img src={catherineImage} alt="Catherine" />
               </div>
-              <div className="card-content">
-                <span className="card-title">Full Stack Web Developer</span>
-                <span className="card-name">Catherine Sarquís</span>
-                <p className="card-text">
+              <div className={styles.teamMemberContent}>
+                <span className={styles.teamMemberTitle}>
+                  Software Developer
+                </span>
+                <h3 className={styles.teamMemberName}>Catherine Sarquís</h3>
+                <p className={styles.teamMemberDescription}>
                   Catherine is transitioning into software development, driven
                   by her passion for detail and problem-solving.
                   Catherine&apos;s experience in proofreading, digital editing
                   and love of languages bring a unique perspective to her work.
                 </p>
-                <button className="card-btn">View More</button>
+                <button className={styles.readMoreBtn}>View More</button>
+              </div>
+            </div>
+            <div className={styles.teamMemberCard}>
+              <div className={styles.teamMemberImage}>
+                <img src={davidImage} alt="David" />
+              </div>
+              <div className={styles.teamMemberContent}>
+                <span className={styles.teamMemberTitle}>
+                  Software Developer
+                </span>
+                <h3 className={styles.teamMemberName}>David A Silva</h3>
+                <p className={styles.teamMemberDescription}>
+                  Passionate about creating software and web applications, David
+                  is transitioning from music to tech with determination and
+                  exploring creativity through code.
+                </p>
+                <button className={styles.readMoreBtn}>View More</button>
+              </div>
+            </div>
+            <div className={styles.teamMemberCard}>
+              <div className={styles.teamMemberImage}>
+                <img src={raviImage} alt="Ravi" />
+              </div>
+              <div className={styles.teamMemberContent}>
+                <span className={styles.teamMemberTitle}>
+                  Software Developer
+                </span>
+                <h3 className={styles.teamMemberName}>Ravi Kataria</h3>
+                <p className={styles.teamMemberDescription}>
+                  Hi, I&apos;m Ravi! I come from a background in education and
+                  am currently transitioning into technology. I love the
+                  problem-solving nature of the tech field and ca&apos;t wait to
+                  use my knowledge to devise solutions!
+                </p>
+                <button className={styles.readMoreBtn}>View More</button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="card">
-        <div className="profile-circle">
-          <img src="images/david-small.jpg" alt="David" />
-        </div>
-        <h3>David</h3>
-        <p>
-          Passionate about creating software and web applications, David is
-          transitioning from music to tech with determination and exploring
-          creativity through code.
-        </p>
-      </div>
-
-      <div className="card">
-        <div className="profile-circle">
-          <img src="images/ravi-small.jpg" alt="Ravi" />
-        </div>
-        <h3>Ravi</h3>
-        <p>
-          Hi, I&apos;m Ravi! I come from a background in education and am
-          currently transitioning into technology. I love the problem-solving
-          nature of the tech field and ca&apos;t wait to use my knowledge to
-          devise solutions!
-        </p>
       </div>
     </section>
   );
