f85733b83d463fa3ba8bfbb5d93fb5af805a1efa
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-25 10:57:32 +0100

    restart1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import "./App.css";
-import Nav from "./Nav/Nav";
-import Hero from "./Hero/Hero";
-import About from "./About/About";
-import OurStory from "./OurStory/OurStory";
-import OurTeam from "./OurTeam/OurTeam";
-import Projects from "./Projects/Projects";
-import Contact from "./Contact/Contact";
-import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <>
-      <Nav />
-      <Hero />
-      <About />
-      <OurStory />
-      <OurTeam />
-      <Projects />
-      <Contact />
-      <Footer />
+      <h1>restart!</h1>
     </>
   );
 }
