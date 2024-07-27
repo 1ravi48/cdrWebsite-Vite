3871fa6ddf2837cfc13d7d69fa01be8ed8a5b432
csarq <catherinesarquis@gmail.com>
2024-07-27 10:50:54 +0100

    play with font weights

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import styles from "./App.module.css";
+
 import Nav from "./Nav/Nav";
 
 function App() {
   return (
     <>
       <Nav />
+      <p className={styles.para}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure ea quas
+        placeat tempore? Dolores dolore natus veritatis in? Aspernatur,
+        voluptate! Esse voluptas debitis perferendis odit velit earum eligendi
+        quam tempora?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure ea quas
+        placeat tempore? Dolores dolore natus veritatis in? Aspernatur,
+        voluptate! Esse voluptas debitis perferendis odit velit earum eligendi
+        quam tempora?
+      </p>
     </>
   );
 }
