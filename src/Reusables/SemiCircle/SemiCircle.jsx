750c7b06d79eb1920145599e71766729e21a908b
csarq <catherinesarquis@gmail.com>
2024-07-27 10:29:05 +0100

    add semicircle component

---
+++ src/Reusables/SemiCircle/SemiCircle.jsx
@@ -0,0 +1,28 @@
+const SemiCircle = () => {
+  return (
+    <svg
+      width="72"
+      height="36"
+      viewBox="0 0 72 72"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 36 A36 36 0 0 1 72 36 Z" fill="url(#paint0_linear_214_647)" />
+      <defs>
+        <linearGradient
+          id="paint0_linear_214_647"
+          x1="72"
+          y1="0"
+          x2="0"
+          y2="72"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F76680" />
+          <stop offset="1" stopColor="#57007B" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default SemiCircle;
