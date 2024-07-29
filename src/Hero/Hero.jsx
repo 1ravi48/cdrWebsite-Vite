dd0465fd438951d7813ec0f09864bcd8067cbdc1
csarq <catherinesarquis@gmail.com>
2024-07-29 09:00:33 +0100

    start hero section

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,4 +1,26 @@
-export default function Nav() {
-    return <></>;
-  }
-  
+import styles from "./Hero.module.css";
+import heroImage from "../assets/images/hero_image.svg";
+
+export default function Hero() {
+  return (
+    <>
+      <section href="#home">
+        <h1>
+          <span className={styles.thin}>
+            No&nbsp;
+            <span className={styles.textGradient}>Website?</span>
+          </span>
+          <br />
+          No <span className={styles.textGradient}>Problem.</span>
+        </h1>
+
+        <p className={styles.heroPara}>
+          CDR Devs create simple, effective websites that get your business
+          online – fast.
+        </p>
+
+        <img src={heroImage} alt="Image by vectorjuice on Freepik" />
+      </section>
+    </>
+  );
+}
