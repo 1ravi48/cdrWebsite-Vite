81629343a074ba2c63d3bf20cbb1403548655267
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-29 13:59:38 +0100

    OurTeam code block added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import "./App.css";
-
 import Nav from "./Nav/Nav";
-import SectionTitle from "./Reusables/SectionTitle/SectionTitle";
-import MemberProfile from "./Reusables/MemberProfile/MemberProfile";
-import Ravi from "./assets/images/ravi-small.jpg";
 import Hero from "./Hero/Hero";
+import OurTeam from "./OurTeam/OurTeam";
 
 function App() {
   return (
     <div className="container">
       <Nav />
       <Hero />
-
-      <SectionTitle title="About Us" />
-      <MemberProfile
-        image={Ravi}
-        name="Ravi Kataria"
-        title="Software Developer"
-        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptate sit dolorem sed esse sequi facilis repudiandae officia recusandae quae? Consequuntur architecto nobis enim ipsam quis qui, aliquid quo quidem?"
-      />
+      <OurTeam />
     </div>
   );
 }
