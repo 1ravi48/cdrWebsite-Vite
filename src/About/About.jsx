0d58887aada0583d9037d97f47551ea6621f6b9d
1ravi48 <ravi48@hotmail.co.uk>
2024-07-29 16:01:58 +0100

    started the About Us section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,3 +1,26 @@
-export default function Nav() {
-  return <></>;
+import SectionTitle from "../Reusables/SectionTitle/SectionTitle.jsx";
+import SemiCircle from "../Reusables/SemiCircle/SemiCircle";
+
+export default function About() {
+  return (
+    <>
+      <section href="#about">
+        <SectionTitle title="About Us" />
+        <p>
+          Our journey as CDR Devs started with a shared dream: to use our coding
+          skills to have a positive impact on the world 🔥.
+        </p>
+        <p>
+          We're a team of passionate developers who believe that a well-crafted
+          website can be a powerful tool for growth and success.
+        </p>
+        <p>
+          Whether you're a small business or a freelance creative, we're here to
+          help you reach your full online potential.
+        </p>
+        <p>Let's create something extraordinary!</p>
+        <SemiCircle />
+      </section>
+    </>
+  );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Nav from "./Nav/Nav";
 import Hero from "./Hero/Hero";
 import OurTeam from "./OurTeam/OurTeam";
+import About from "./About/About";
 
 function App() {
   return (
     <div className="container">
       <Nav />
       <Hero />
+      <About />
       <OurTeam />
     </div>
   );
