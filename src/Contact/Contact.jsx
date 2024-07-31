1d3e740ddfb1bb396aac614a429991837d9bc15d
DavidAdam1323 <davidadamgtr@gmail.com>
2024-07-31 10:47:29 +0100

    Contact.jsx code block added.

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,4 +1,50 @@
-export default function Nav() {
-    return <></>;
-  }
-  
+import styles from "./Contact.module.css";
+import SectionTitle from "../Reusables/SectionTitle/SectionTitle";
+import SemiCircle from "../Reusables/SemiCircle/SemiCircle";
+import Button from "../Reusables/Button/Button";
+
+function Contact() {
+  return (
+    <>
+      <section id="contact">
+        <SectionTitle title="Contact Us" />
+        <p>Ready to elevate your business?</p>
+        <p>
+          We are here to help! Reach out today to discuss how we can make your
+          online presence shine. We are excited to hear your ideas and achieve
+          your goals together!
+        </p>
+        <div className={styles.SectionFormContainer}>
+          <div className={styles.formContainer}>
+            <form action="" method="post">
+              <div className={styles.formGroup}>
+                <label htmlFor="name">🙎🏻‍♂️ Name</label>
+                <div className={styles.gradientBg}>
+                  <input type="text" name="name" id="name" required />
+                </div>
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="email">📧 Email</label>
+                <div className={styles.gradientBg}>
+                  <input type="text" name="email" id="email" required />
+                </div>
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="message">💭 Message</label>
+                <div className={styles.gradientBgText}>
+                  <textarea name="message" id="message" required></textarea>
+                </div>
+              </div>
+              <div className={styles.formBtnContainer}>
+                <Button btnText="Submit 📩" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <SemiCircle />
+      </section>
+    </>
+  );
+}
+
+export default Contact;
