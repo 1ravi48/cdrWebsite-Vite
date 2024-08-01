e247224ca410800f44c3cb54b329ea9c2baa949e
csarq <catherinesarquis@gmail.com>
2024-08-01 11:42:01 +0100

    Re-write About Us section to make us more relatable to users

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -9,18 +9,34 @@ export default function About() {
       <section id="about">
         <SectionTitle title="About Us" />
         <p>
-          Our journey as CDR Devs started with a shared dream: to use our coding
-          skills to have a positive impact on the world 🔥.
+          Imagine a team of frontend developers with the skills to tackle any
+          challenge and the experience to deliver results. That&apos;s us: CDR
+          Devs. We&apos;re a tight-knit team of three developers, united by our
+          experience in a rigorous software development bootcamp, eager to put
+          our knowledge to work for you.
         </p>
         <p>
-          We&apos;re a team of passionate developers who believe that a
-          well-crafted website can be a powerful tool for growth and success.
+          We&apos;re not just coders; we&apos;re problem solvers, collaborators,
+          and passionate creators. Our primary goal is to understand your needs,
+          to craft tailored solutions, and deliver exceptional results.
         </p>
         <p>
-          Whether you&apos;re a small business or a freelance creative,
-          we&apos;re here to help you reach your full online potential.
+          Whether you need a brand-new website, a redesign of an existing one,
+          or just some frontend tweaks, we&apos;re here to help. We&apos;ll work
+          closely with you to understand your vision, goals, and challenges. We
+          believe that a well-crafted website is more than just an online
+          presence; it&apos;s a powerful tool for growth and success.
         </p>
-        <p>Let&apos;s create something extraordinary!</p>
+        <p>
+          Whether you&apos;re a budding entrepreneur or an established business,
+          we&apos;re here to help you unlock your full digital potential and
+          create a website that truly reflects your vision.
+        </p>
+        <p>
+          Partner with CDR Devs and experience the difference a team of
+          dedicated frontend developers can make.
+        </p>
+
         <img
           src={aboutImage}
           className={styles.aboutImage}
