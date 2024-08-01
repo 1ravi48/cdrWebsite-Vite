c80d30083ae3908f8b27dca8ba7dac57a06d6758
DavidAdam1323 <davidadamgtr@gmail.com>
2024-08-01 20:39:40 +0100

    Add ServicesCard component with PropTypes validation.

---
+++ src/Reusables/ServicesCard/ServicesCard.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import styles from "./ServicesCard.module.css";
+
+const ServicesCard = ({
+  image,
+  name,
+  subtitle,
+  description,
+  imgBg,
+  textBg,
+}) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.imageContainer} style={{ backgroundColor: imgBg }}>
+        <img src={image} alt={name} className={styles.servicesImage} />
+      </div>
+      <div
+        className={styles.descriptionContainer}
+        style={{ backgroundColor: textBg }}
+      >
+        <b>{subtitle}</b>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+ServicesCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgBg: PropTypes.string,
+  textBg: PropTypes.string,
+};
+
+export default ServicesCard;
