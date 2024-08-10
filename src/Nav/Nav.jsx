3cbf414d8985d07c3e951c224a06ef5efb236f7b
csarq <catherinesarquis@gmail.com>
2024-08-10 15:44:49 +0100

    playing with nav bar

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -65,109 +65,115 @@ export default function Nav() {
           </li>
         </ul>
       </nav>
+      {/* Mobile Nav */}
       <nav className={styles.NavContainer}>
-        <a href="#home">
-          <img src={logo} className={styles.logo}></img>
-        </a>
-        <div
-          className={styles.HamburgerContainer}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? (
-            <img
-              className={styles.HamburgerIcon}
-              src={hamburgerCross}
-              alt="hamburger-icon"
-            />
-          ) : (
-            <img
-              className={styles.HamburgerIcon}
-              src={hamburgerIcon}
-              alt="hamburger-icon"
-            />
+        <div className={styles.MobContainer}>
+          <a href="#home">
+            <img src={logo} className={styles.logo}></img>
+          </a>
+          <div
+            className={styles.HamburgerContainer}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <img
+                className={styles.HamburgerIcon}
+                src={hamburgerCross}
+                alt="hamburger-icon"
+              />
+            ) : (
+              <img
+                className={styles.HamburgerIcon}
+                src={hamburgerIcon}
+                alt="hamburger-icon"
+              />
+            )}
+          </div>
+
+          {isOpen && (
+            <ul className={styles.MenuLinks}>
+              <li
+                className={
+                  homeHovering ? styles.ListItemHover : styles.ListItemNotHover
+                }
+              >
+                <a
+                  href="#home"
+                  className={homeHovering ? styles.Hover : styles.NotHover}
+                  onMouseEnter={() => setHomeHovering(true)}
+                  onMouseLeave={() => setHomeHovering(false)}
+                  onClick={navHomeClick}
+                >
+                  Home
+                </a>
+              </li>
+              <li
+                className={
+                  aboutHovering ? styles.ListItemHover : styles.ListItemNotHover
+                }
+              >
+                <a
+                  href="#about"
+                  className={aboutHovering ? styles.Hover : styles.NotHover}
+                  onMouseEnter={() => setAboutHovering(true)}
+                  onMouseLeave={() => setAboutHovering(false)}
+                  onClick={navAboutClick}
+                >
+                  About Us
+                </a>
+              </li>
+              <li
+                className={
+                  servicesHovering
+                    ? styles.ListItemHover
+                    : styles.ListItemNotHover
+                }
+              >
+                <a
+                  href="#services"
+                  className={servicesHovering ? styles.Hover : styles.NotHover}
+                  onMouseEnter={() => setServicesHovering(true)}
+                  onMouseLeave={() => setServicesHovering(false)}
+                  onClick={navServicesClick}
+                >
+                  Our Services
+                </a>
+              </li>
+              <li
+                className={
+                  teamHovering ? styles.ListItemHover : styles.ListItemNotHover
+                }
+              >
+                <a
+                  href="#team"
+                  className={teamHovering ? styles.Hover : styles.NotHover}
+                  onMouseEnter={() => setTeamHovering(true)}
+                  onMouseLeave={() => setTeamHovering(false)}
+                  onClick={navTeamClick}
+                >
+                  Our Team
+                </a>
+              </li>
+              <li
+                className={
+                  contactHovering
+                    ? styles.ListItemHover
+                    : styles.ListItemNotHover
+                }
+              >
+                <a
+                  href="#contact"
+                  className={contactHovering ? styles.Hover : styles.NotHover}
+                  onMouseEnter={() => setContactHovering(true)}
+                  onMouseLeave={() => setContactHovering(false)}
+                  onClick={navContactClick}
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
           )}
         </div>
-        {isOpen && (
-          <ul className={styles.MenuLinks}>
-            <li
-              className={
-                homeHovering ? styles.ListItemHover : styles.ListItemNotHover
-              }
-            >
-              <a
-                href="#home"
-                className={homeHovering ? styles.Hover : styles.NotHover}
-                onMouseEnter={() => setHomeHovering(true)}
-                onMouseLeave={() => setHomeHovering(false)}
-                onClick={navHomeClick}
-              >
-                Home
-              </a>
-            </li>
-            <li
-              className={
-                aboutHovering ? styles.ListItemHover : styles.ListItemNotHover
-              }
-            >
-              <a
-                href="#about"
-                className={aboutHovering ? styles.Hover : styles.NotHover}
-                onMouseEnter={() => setAboutHovering(true)}
-                onMouseLeave={() => setAboutHovering(false)}
-                onClick={navAboutClick}
-              >
-                About Us
-              </a>
-            </li>
-            <li
-              className={
-                servicesHovering
-                  ? styles.ListItemHover
-                  : styles.ListItemNotHover
-              }
-            >
-              <a
-                href="#services"
-                className={servicesHovering ? styles.Hover : styles.NotHover}
-                onMouseEnter={() => setServicesHovering(true)}
-                onMouseLeave={() => setServicesHovering(false)}
-                onClick={navServicesClick}
-              >
-                Our Services
-              </a>
-            </li>
-            <li
-              className={
-                teamHovering ? styles.ListItemHover : styles.ListItemNotHover
-              }
-            >
-              <a
-                href="#team"
-                className={teamHovering ? styles.Hover : styles.NotHover}
-                onMouseEnter={() => setTeamHovering(true)}
-                onMouseLeave={() => setTeamHovering(false)}
-                onClick={navTeamClick}
-              >
-                Our Team
-              </a>
-            </li>
-            <li
-              className={
-                contactHovering ? styles.ListItemHover : styles.ListItemNotHover
-              }
-            >
-              <a
-                href="#contact"
-                className={contactHovering ? styles.Hover : styles.NotHover}
-                onMouseEnter={() => setContactHovering(true)}
-                onMouseLeave={() => setContactHovering(false)}
-                onClick={navContactClick}
-              >
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        )}
       </nav>
     </>
   );
